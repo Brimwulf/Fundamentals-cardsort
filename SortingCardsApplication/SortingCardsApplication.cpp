9f6d24fcb3a7382f27fcf947fde4cecaf1380cad
vl024813 <vl024813@student.reading.ac.uk>
2024-03-18 15:59:01 +0000

    fixed the displaying of cards + suits

--- SortingCardsApplication/SortingCardsApplication.cpp
+++ SortingCardsApplication/SortingCardsApplication.cpp
@@ -14,16 +14,21 @@ aCard thePack[maxCard];
 
 
 int main() {
-    cout << "Card Sorting!\n";
+    cout << "Card Sorting!\n" << endl;
 
     for (int ct = 0; ct < maxCard; ct++) {
         thePack[ct] = getCard("32024813");   // change to your student number
         //cout << thePack[ct].cardVal << " of " << thePack[ct].cardSuit << endl;
-        if (thePack[ct].cardVal == 1) cout << "Ace";
-        else if (thePack[ct].cardVal == 11) cout << "Jack";
-        else if (thePack[ct].cardVal == 12) cout << "Queen";
-        else if (thePack[ct].cardVal == 11) cout << "King";
-        else cout << thePack[ct].cardVal;
-        cout << " of " << thePack[ct].cardSuit << endl;
+        if (thePack[ct].cardVal == 1) cout << "Ace" << " of ";
+        else if (thePack[ct].cardVal == 11) cout << "Jack" << " of ";
+        else if (thePack[ct].cardVal == 12) cout << "Queen" << " of ";
+        else if (thePack[ct].cardVal == 13) cout << "King" << " of ";
+        else cout << thePack[ct].cardVal << " of ";
+        if (thePack[ct].cardSuit == 0) cout << "clubs";
+        else if (thePack[ct].cardSuit == 1) cout << "diamonds";
+        else if (thePack[ct].cardSuit == 2) cout << "hearts";
+        else if (thePack[ct].cardSuit == 3) cout << "spades";
+        else cout << thePack[ct].cardSuit;
+        cout << endl;
     }
 }
